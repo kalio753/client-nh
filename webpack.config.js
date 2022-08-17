23c0ffeae8436579433e9047d3a23833dcde6e0a
kalio753 <duongchikien2001@gmail.com>
2022-08-17 09:14:37 +0700

    Update css n scss loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,14 @@ module.exports = {
                     },
                 },
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
         ],
     },
 }
